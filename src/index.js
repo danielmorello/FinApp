6e7a316d80765add30704f9ec4a30d7743046daa
dmorello <daniel.morello@compsis.com.br>
2022-08-14 01:12:01 -0300

    primeiro commit do porjeto

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { v4: uuid } = require("uuid");
+
+const app = express();
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customersAlreadyExists = customers.some(
+    (customers) => customers.cpf === cpf
+  );
+
+  if (customersAlreadyExists) {
+    return response.status(400).json({ error: "Customers already exists" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuid(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
+
+app.get("/statement", (request, response) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customers) => customers.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" });
+  }
+
+  return response.json(customer.statement);
+});
+
+app.listen(3333);
